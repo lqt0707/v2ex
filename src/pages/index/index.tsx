4129f2c43ac45f764fba93f44ab13e35def70fcb
liuqiutao <qiutao.liu@7x-networks.com>
2023-02-07 18:34:23 +0800

    feat(all): first commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,36 @@
+import { IThread } from '../../interfaces/thread';
+import React, { useEffect, useState } from 'react';
+import api from '../../utils/api';
+import Taro from '@tarojs/taro';
+import { View } from '@tarojs/components';
+import ThreadList from '../../components/thread/thread_list';
+
+interface IState {
+  loading: boolean;
+  threads: IThread[];
+}
+const Index: React.FC<IState> = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [threads, setThreads] = useState<IThread[]>([]);
+
+  useEffect(async () => {
+    try {
+      const res = await Taro.request<IThread[]>({
+        url: api.getLatestTopic(),
+      });
+      setLoading(false);
+      setThreads(res.data);
+    } catch (e) {
+      Taro.showToast({
+        title: '载入远程数据错误',
+      });
+    }
+  }, []);
+
+  return (
+    <View className={'index'}>
+      <ThreadList threads={threads} loading={loading} />
+    </View>
+  );
+};
+export default Index;
